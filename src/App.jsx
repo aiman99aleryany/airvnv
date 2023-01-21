acebc5c1f3c1aa9e225a4dfa1ba3f3c15ec9d321
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2023-01-21 06:56:41 +0200

    Folder Structure is built

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/Index.scss";
+import NavBar from "./components/navbar/NavBar";
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
