75cac7b9f6eec6b85a3e3b6c160f4bdc82bb29d3
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2023-01-21 08:30:45 +0200

    Best component design is done

---
+++ src/components/best/Best.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Apt1 from '../../assets/apartment1.jpg';
+import Apt2 from '../../assets/apartment2.jpg';
+import Apt3 from '../../assets/apartment3.jpg';
+
+import './Best.scss';
+
+function Best() {
+    return (
+        <div className="best">
+            <h1>Find best Rated properties</h1>
+            <div>
+                <p>
+                    <span className="bold">All</span>
+                </p>
+                <p>Commercial</p>
+                <p>Residential</p>
+                <p>Agricultural</p>
+            </div>
+            <div className="container">
+                <img src={Apt1} alt="1" />
+                <img src={Apt2} alt="2" />
+                <img src={Apt3} alt="3" />
+            </div>
+            <button className="btn">View All</button>
+        </div>
+    );
+}
+
+export default Best;
