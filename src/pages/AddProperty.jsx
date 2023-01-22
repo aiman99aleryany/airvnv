bb6f3c6946f6cd24e4e64e5cea73e5d402356019
Arya Karimi <devaryakrm@gmail.com>
2023-01-22 14:10:04 +0300

    sign in page done

--- src/pages/AddProperty.jsx
+++ src/pages/AddProperty.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import AddPropertyForm from '../components/addPropertyForm/AddPropertyFrom';
 import NavBar from '../components/navbar/NavBar';
-import { ScrollRestoration } from 'react-router-dom';
+import { Link, ScrollRestoration } from 'react-router-dom';
+import { getLocalStorage } from '../store/localStorage';
 
 function AddProperty() {
+    const isUserSignIn = getLocalStorage('isUserSignedIn');
     return (
         <div>
             <NavBar />
-            <AddPropertyForm />
+            {isUserSignIn ? (
+                <AddPropertyForm />
+            ) : (
+                <Link to={'/signin'}>
+                    <button className="btn">Sign in to add property</button>
+                </Link>
+            )}
+
             <ScrollRestoration />
         </div>
     );
